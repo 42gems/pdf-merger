b27c7ced1a0745b70c092c5c6f4e87b834d4d131
bogdasya <bogdasya93@gmail.com>
2014-10-31 15:59:01 +0200

    Delete method home and fix string

--- app/controllers/pdf_merge_controller.rb
+++ app/controllers/pdf_merge_controller.rb
@@ -1,10 +1,6 @@
 class PdfMergeController < ApplicationController
 
-  def home
-  end
-
   def upload
-     send_file MergePDF.merge(params[:file1], params[:file2]),
-               type: "application/pdf"
+     send_file MergePDF.merge(params[:file1], params[:file2]), type: "application/pdf"
   end
 end
