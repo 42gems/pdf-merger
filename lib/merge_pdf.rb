208af86f28635a549fd341dcb144b7b8a8d7d75b
bogdasya <bogdasya93@gmail.com>
2014-11-03 11:26:46 +0200

    Adding SecureRandom.hex

--- lib/merge_pdf.rb
+++ lib/merge_pdf.rb
@@ -1,10 +1,11 @@
 module MergePDF
+ require "securerandom"
 
   def self.merge(data)
     path = "merged_pdfs/assignment.pdf"
     pdf = PDF::Merger.new
     data.each { |file_path| pdf.add_file file_path.path }
-    pdf.save_as path
+    pdf.save_as path + SecureRandom.hex(4)
     path
     end
   end
