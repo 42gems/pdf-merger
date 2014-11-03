8c3e609eb3d9caf1cd150b13a0ec858b103883a7
bogdasya <bogdasya93@gmail.com>
2014-11-03 11:35:42 +0200

    Delete SecureRandom

--- lib/merge_pdf.rb
+++ lib/merge_pdf.rb
@@ -1,11 +1,10 @@
 module MergePDF
- require "securerandom"
 
   def self.merge(data)
     path = "merged_pdfs/assignment.pdf"
     pdf = PDF::Merger.new
     data.each { |file_path| pdf.add_file file_path.path }
-    pdf.save_as path + SecureRandom.hex(4)
+    pdf.save_as path
     path
     end
   end
