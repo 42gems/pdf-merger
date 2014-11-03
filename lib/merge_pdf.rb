c88fdb8ae13fd8bd30a1b3aeccdc7811cdf6d227
bogdasya <bogdasya93@gmail.com>
2014-11-03 10:55:45 +0200

    Adding .form_group and delete space

--- lib/merge_pdf.rb
+++ lib/merge_pdf.rb
@@ -3,7 +3,7 @@ module MergePDF
   def self.merge(data)
     path = "merged_pdfs/assignment.pdf"
     pdf = PDF::Merger.new
-    data.each {|file_path| pdf.add_file file_path.path  }
+    data.each {|file_path| pdf.add_file file_path.path}
     pdf.save_as path
     path
     end
