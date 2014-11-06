278941995083e1bb76e9fc1207d805d63d852384
bogdasya <bogdasya93@gmail.com>
2014-11-06 09:58:05 +0200

    Change classes

--- app/controllers/pdf_merge_controller.rb
+++ app/controllers/pdf_merge_controller.rb
@@ -1,6 +1,13 @@
 class PdfMergeController < ApplicationController
 
   def upload
+     check(params[:files])
      send_file MergePDF.merge(params[:files]), type: "application/pdf"
   end
+
+  def check(data)
+  #  debugger
+        flash[:notice] = "Post successfully created"
+        redirect_to root_path
+    end
 end
