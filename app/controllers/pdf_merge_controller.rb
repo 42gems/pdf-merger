7a492ae41696d7c501628d8caaf264d136ccfc88
bogdasya <bogdasya93@gmail.com>
2014-11-07 10:24:04 +0200

    Delete check

--- app/controllers/pdf_merge_controller.rb
+++ app/controllers/pdf_merge_controller.rb
@@ -4,10 +4,4 @@ class PdfMergeController < ApplicationController
      check(params[:files])
      send_file MergePDF.merge(params[:files]), type: "application/pdf"
   end
-
-  def check(data)
-  #  debugger
-        flash[:notice] = "Post successfully created"
-        redirect_to root_path
-    end
 end
