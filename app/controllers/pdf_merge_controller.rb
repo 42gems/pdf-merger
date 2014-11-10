b2f3bbabd8365f7d264c3fecc564686cc460ed5a
bogdasya <bogdasya93@gmail.com>
2014-11-10 13:37:54 +0200

    Adding pdf validate

--- app/controllers/pdf_merge_controller.rb
+++ app/controllers/pdf_merge_controller.rb
@@ -1,6 +1,19 @@
 class PdfMergeController < ApplicationController
+  before_filter :check, only:[:upload]
 
   def upload
      send_file MergePDF.merge(params[:files]), type: "application/pdf"
+  end
+
+  private
+  def check
+    data = params[:files]
+    #debugger
+    data.each do |file|
+      unless  file.content_type =~ /.pdf$/
+        flash[:notice] = "Files must be .pdf"
+        redirect_to :back
+      end
+    end
   end
 end
