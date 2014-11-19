57c5831bd70ade3c0693fcccf17a6faae27aa23b
bogdasya <bogdasya93@gmail.com>
2014-11-19 18:20:26 +0200

    One scenario - one exprct

--- spec/features/buttons_add_deletes_spec.rb
+++ spec/features/buttons_add_deletes_spec.rb
@@ -4,12 +4,13 @@ feature 'Tests for main page' do
   background do
     visit(root_path)
   end
-  scenario 'User should check content on page' do
+
+  scenario 'User should see PDF Merger' do
     expect(page).to have_title 'PDF Merger'
+  end
+
+  scenario 'User should see add button' do
     expect(page).to have_selector('button#add',visible: true)
-    expect(page).to have_selector('button.delete',visible: false)
-    expect(page).to have_selector('input')
-    expect(page).to have_selector(' button.btn',visible: true)
   end
 
   scenario 'User adds new row ', js: true do
@@ -18,10 +19,21 @@ feature 'Tests for main page' do
     page.should have_css('button.delete', count: 3)
   end
 
+  scenario 'User should see delete button' do
+    expect(page).to have_selector('button.delete',visible: false)
+  end
+
   scenario 'User deletes row', js: true do
     find_by_id('add').click
     first(:button, 'Delete').click
     page.should have_css('table.table tr', count: 2)
     page.should have_css('button.delete', count: 0)
   end
+
+  scenario 'User should see input form' do
+    expect(page).to have_selector('input')
+    expect(page).to have_selector(' button.btn',visible: true)
+  end
+
+
 end
